fcaa23fc19a8b847c9f384bb69324304c8eb6519
DESKTOP-3JVU7BH\janghoon <jhhwang@mappers.kr>
2021-09-23 09:12:50 +0900

    학습진행 시작 부분과 각종 메소드 추가

--- Mnist_train.py
+++ Mnist_train.py
@@ -1,14 +1,25 @@
 import torch
 from torchvision import datasets, transforms
+import torch.optim as optim
+import torch.nn as nn
+from Net import Net
 
-if __name__ == "__main__":
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.1307,),(0.3081,))
-    ])
+'''데이터 로더'''
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,),(0.3081,))
+])
 
-    train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('data', train=True, download=True, transform=transform))
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('data', train=True, download=True, transform=transform))
 
-    test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('data', train=False, trasforms=transforms))
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('data', train=False, transform=transform))
+
+'''학습 시작'''
+model = Net()
+
+optimizer = optim.SGD(model.parameters(), lr=0.001)
+criterion = nn.BCEWithLogitsLoss()
+
+model.train(train_loader, optimizer, criterion)
